0e2b19e0011955a5e6e9b70b62a3657f18c1764d
dinalailil <dinarullailil26@gmail.com>
2025-10-22 13:04:40 +0700

    UTS: Selesai Soal 1 - ListTile dan Navigasi Balik

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 class ResultScreen extends StatelessWidget {
   final String ocrText;
@@ -15,10 +16,25 @@ class ResultScreen extends StatelessWidget {
           child: SelectableText(
             ocrText.isEmpty
                 ? 'Tidak ada teks ditemukan.'
-                : ocrText.replaceAll('\n', ' '),
+                : ocrText, // tampilkan teks dengan baris baru utuh
             style: const TextStyle(fontSize: 18),
           ),
         ),
+      ),
+
+      // FloatingActionButton untuk kembali ke HomeScreen
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blueAccent, // Warna tombol Home
+        foregroundColor: Colors.white,       // Warna ikon jadi putih
+        tooltip: 'Kembali ke Home',
+        onPressed: () {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (_) => const HomeScreen()),
+            (Route<dynamic> route) => false,
+          );
+        },
+        child: const Icon(Icons.home, size: 28),
       ),
     );
   }
